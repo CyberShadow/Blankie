176479b754e93f1a7ca0144dece877c59f9e5954
Vladimir Panteleev <git@cy.md>
2022-05-01 08:43:25 +0000

    xssmgr.modules.xset: Fix for empty config

--- src/xssmgr/modules/xset.py
+++ src/xssmgr/modules/xset.py
@@ -20,11 +20,11 @@ def mod_xset(*args):
 			max_timeout = 0
 			for (timeout, _module) in xssmgr.config.configurator.on_idle_modules:
 				if timeout <= 0:
-					log('mod_xset: Invalid idle time: %d, ignoring', timeout)
+					log('mod_xset: Invalid idle time: %s, ignoring', timeout)
 					continue
 				min_timeout = min(min_timeout, timeout)
 				max_timeout = max(max_timeout, timeout)
-			logv('mod_xset: Configuring X screensaver for idle hooks in the %d .. %d range.',
+			logv('mod_xset: Configuring X screensaver for idle hooks in the %s .. %s range.',
 				 min_timeout, max_timeout)
 			if max_timeout > 0:
 				subprocess.check_call(['xset', 's', str(min_timeout), '0'])
