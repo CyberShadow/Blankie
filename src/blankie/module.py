0f672faafba884ef5101f31a1608854d3395b17e
Vladimir Panteleev <git@cy.md>
2023-03-10 23:02:40 +0000

    blankie.module: Fix modules being imported un-namespaced
    This prevented a "blankie.modules.dbus" module from importing the
    "dbus" package.

--- src/blankie/module.py
+++ src/blankie/module.py
@@ -78,7 +78,7 @@ def load_module(module_name):
 			# https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
 			spec = importlib.util.spec_from_file_location(python_module_name, module_file)
 			module = importlib.util.module_from_spec(spec)
-			sys.modules[module_name] = module
+			sys.modules[python_module_name] = module
 			spec.loader.exec_module(module)
 
 			return
