1cd44e8f5f34a698584fb42bb36e1cbb0f0994df
Vladimir Panteleev <git@cy.md>
2023-03-11 06:21:37 +0000

    blankie.modules.logind: Integrate with inhibitor locks
    The inhibitor lock allows us to integrate with the login manager to
    synchronously put the system in a properly locked state before it goes
    to sleep.

--- src/blankie/modules/logind.py
+++ src/blankie/modules/logind.py
@@ -1,6 +1,10 @@
 # blankie.modules.logind - systemd-logind integration
 # Used to reliably lock the system when it goes to sleep.
 
+import os
+
+import dbus
+
 import blankie
 
 class LogindModule(blankie.module.Module):
@@ -14,6 +18,7 @@ class LogindModule(blankie.module.Module):
 	def __init__(self):
 		super().__init__()
 		self.dbus = blankie.module.get(self.DBUS_SPEC)
+		self.inhibitor_lock = None
 
 	def start(self):
 		def setup():
@@ -23,6 +28,7 @@ class LogindModule(blankie.module.Module):
 				dbus_interface='org.freedesktop.login1.Manager',
 				path='/org/freedesktop/login1',
 			)
+			self.inhibit()
 		self.dbus.glib.run_sync(setup)
 
 	def stop(self):
@@ -33,13 +39,59 @@ class LogindModule(blankie.module.Module):
 				dbus_interface='org.freedesktop.login1.Manager',
 				path='/org/freedesktop/login1',
 			)
+			if self.inhibitor_lock is not None:
+				self.uninhibit()
 		self.dbus.glib.run_sync(teardown)
+
+	# Runs in the GLib main loop thread:
+	def inhibit(self):
+		assert self.inhibitor_lock is None
+		obj = self.dbus.system_bus.get_object(
+			bus_name='org.freedesktop.login1',
+			object_path='/org/freedesktop/login1',
+		)
+		unix_fd = obj.Inhibit(
+			"sleep",
+			"Blankie",
+			"Lock the system before sleep",
+			"delay",
+			dbus_interface='org.freedesktop.login1.Manager',
+		)
+		self.inhibitor_lock = unix_fd.take()
+
+	# Safe to run from either thread
+	def uninhibit(self):
+		assert self.inhibitor_lock is not None
+		self.log.debug('Releasing inhibitor lock.')
+		os.close(self.inhibitor_lock)
+		self.inhibitor_lock = None
 
 	# Runs in the GLib main loop thread:
 	def handle_sleep_signal(self, start):
 		self.log.debug('System is %s sleep' % ('entering' if start else 'exiting'))
-		if not start:
+		if start:
+			blankie.daemon.call(self.handle_enter_sleep)
+		else:
+			if self.inhibitor_lock is None:
+				self.inhibit()
+			else:
+				self.log.warning('System is exiting sleep but we are already holding an inhibitor lock?')
 			blankie.daemon.call(self.handle_exit_sleep)
 
+	# Runs in the main thread:
+	def handle_enter_sleep(self):
+		# Reconfigure the system appropriately
+		blankie.state.sleeping = True
+		blankie.module.update()
+		# Release the inhibitor lock
+		# This must be done only after the above
+		if self.inhibitor_lock is not None:
+			self.uninhibit()
+		else:
+			self.log.warning('System is going to sleep but we are not holding an inhibitor lock?')
+
+	# Runs in the main thread:
 	def handle_exit_sleep(self):
-		pass  # TODO
+		# Reconfigure the system appropriately
+		blankie.state.sleeping = False
+		blankie.module.update()
